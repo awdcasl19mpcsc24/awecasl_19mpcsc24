ce0d763af09271fd259833b87064ff541866e053
awdcasl19mpcsc24 <86602736+awdcasl19mpcsc24@users.noreply.github.com>
2021-06-30 20:01:11 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Largestnumber
+ {
+   public static void main(String args[])
+   {
+     Scanner sc=new Scanner(System.in);
+     
+    System.out.println("enter 4 different values");
+     int a=sc.nextInt();
+     int b=sc.nextInt();
+     int c=sc.newInt();
+     int d=sx.newInt();
+  if((a>b)&&(a>c)&&(a>d));
+     System.out.println (a+"is big");
+     elseif((b>a)&&(b>c)&&(b>d));
+     System.out.println(b+"is big");
+     elseif((c>a)&&(c>b)&&(c>d));
+     System.out.println(c+"is big");
+     elseif((d>a)&&(d>b)&&(d>c));
+     System.out.println(d+"is big");
+     else
+     System.out.println(" largest number more than 1 time");
+   }
+  }
